555a4d78878c9e57514660f5a731126e8fdffa9a
ibdursun <i.dursun@outlook.de>
2016-11-20 17:33:16 +0100

    Anforderung erfüllt.

---
+++ 04_objektorientierung/07_abstrakte_klassen/src/tpe/oo/abstrakt/Figur.java
@@ -0,0 +1,14 @@
+package tpe.oo.abstrakt;
+
+
+/**
+ * Abstrakte Klasse Figur.
+ */
+public abstract class Figur {
+
+    /**
+     *  Eine Maske.
+     *  @return kommt auf die Subklasse an.
+     */
+    public abstract double flaeche();
+}
