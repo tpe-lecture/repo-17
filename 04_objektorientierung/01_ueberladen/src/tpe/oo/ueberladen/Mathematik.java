6fb2053c7048aebd3a663ec422bc6c4c9620a6f0
ibdursun <i.dursun@outlook.de>
2016-11-20 16:58:18 +0100

    Anforderung erfüllt.

--- 04_objektorientierung/01_ueberladen/src/tpe/oo/ueberladen/Mathematik.java
+++ 04_objektorientierung/01_ueberladen/src/tpe/oo/ueberladen/Mathematik.java
@@ -8,8 +8,44 @@ public final class Mathematik {
     /**
      * Konstruktor.
      */
-    private Mathematik() {        
+    private Mathematik() {
     }
-    
-    // Bitte fügen Sie Ihre Methoden hier ein
+
+    public static int max(int a, int b) {
+        if (a > b) {
+            return a;
+        }
+        else {
+            return b;
+        }
+    }
+
+    public static int max(int a, int b, int c) {
+
+        if (a > b && a > c) {
+            return a;
+        }
+        else if (b > a && b > c){
+            return b;
+        }
+        else {
+            return c;
+        }
+    }
+
+    public static int max(int a, int b, int c, int d) {
+
+        if (a > b && a > c && a > d) {
+            return a;
+        }
+        else if (b > a && b > c && b > d){
+            return b;
+        }
+        else if (c > a && c > b && c > d){
+            return c;
+        }
+        else {
+            return d;
+        }
+    }
 }
