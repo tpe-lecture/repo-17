52b99c07108f29333eb8bb6bded8f3ce778b3617
ibdursun <i.dursun@outlook.de>
2016-11-20 17:08:17 +0100

    Anforderungen erfüllt.

--- 04_objektorientierung/05_polymorphie/src/tpe/oo/polymorphie/Futterstelle.java
+++ 04_objektorientierung/05_polymorphie/src/tpe/oo/polymorphie/Futterstelle.java
@@ -1,38 +1,20 @@
 package tpe.oo.polymorphie;
 
-import tpe.oo.polymorphie.tiere.Affe;
-import tpe.oo.polymorphie.tiere.Giraffe;
-import tpe.oo.polymorphie.tiere.Gorilla;
+import tpe.oo.polymorphie.tiere.ZooTier;
 
 /**
  * Fütterung der Tiere.
  */
 public class Futterstelle {
 
+
     /**
-     * Füttert den Affen.
-     * 
-     * @param affe Affe, der gefüttert werden soll. 
+     *Füttert alle Tiere die hunrig sind.
+     * @param tiere alle tiere werde gleichzeitig gefüttert.
      */
-    public void gibFutter(Affe affe) {
-        affe.fuettern();
+
+    public void fuetterAlle(ZooTier tiere) {
+        tiere.fuettern();
     }
-    
-    /**
-     * Füttert den Gorilla.
-     * 
-     * @param gorilla Gorilla, der gefüttert werden soll. 
-     */
-    public void gibFutter(Gorilla gorilla) {
-        gorilla.fuettern();
-    }
-    
-    /**
-     * Füttert die Giraffe.
-     * 
-     * @param giraffe Giraffe, die gefüttert werden soll. 
-     */
-    public void gibFutter(Giraffe giraffe) {
-        giraffe.fuettern();
-    }
+
 }
--- 04_objektorientierung/05_polymorphie/src/tpe/oo/polymorphie/ZooSimulation.java
+++ 04_objektorientierung/05_polymorphie/src/tpe/oo/polymorphie/ZooSimulation.java
@@ -3,6 +3,7 @@ package tpe.oo.polymorphie;
 import tpe.oo.polymorphie.tiere.Affe;
 import tpe.oo.polymorphie.tiere.Giraffe;
 import tpe.oo.polymorphie.tiere.Gorilla;
+import tpe.oo.polymorphie.tiere.ZooTier;
 
 /**
  * Simulationsklasse.
@@ -15,31 +16,34 @@ public final class ZooSimulation {
     private ZooSimulation() {
         // keine Objekte benötigt
     }
-    
+
     /**
      * Main-Methode.
-     * 
+     *
      * @param args Kommandozeilen-Argumente.
      */
     public static void main(String[] args) {
         Futterstelle futterstelle = new Futterstelle();
-        
-        Affe charlie = new Affe("Charlie");
-        Gorilla buck = new Gorilla("Buck");
-        Giraffe debbie = new Giraffe("Debbie");
-        
-        System.out.println(charlie);
-        System.out.println(buck);
-        System.out.println(debbie);
-        
+
+        ZooTier[] tiere = new ZooTier[3];
+        tiere[0] = new Affe("charlie");
+        tiere[1] = new Gorilla("Buck");
+        tiere[2] = new Giraffe("Debbie");
+
+        System.out.println(tiere[0]);
+        System.out.println(tiere[1]);
+        System.out.println(tiere[2]);
+
         System.out.println("Fütterung...");
-        
-        futterstelle.gibFutter(charlie);
-        futterstelle.gibFutter(buck);
-        futterstelle.gibFutter(debbie);
-        
-        System.out.println(charlie);
-        System.out.println(buck);
-        System.out.println(debbie);
+
+        int i = 0;
+        while (i < 3) {
+            futterstelle.fuetterAlle(tiere[i]);
+            i++;
+        }
+
+        System.out.println(tiere[0]);
+        System.out.println(tiere[1]);
+        System.out.println(tiere[2]);
     }
 }
