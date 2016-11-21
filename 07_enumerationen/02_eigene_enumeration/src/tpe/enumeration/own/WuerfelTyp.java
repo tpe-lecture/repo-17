e1be38f44b0203cb0f6c867767b484ed3b22a744
ibdursun <i.dursun@outlook.de>
2016-11-21 11:32:51 +0100

    Anforderung erfüllt.

---
+++ 07_enumerationen/02_eigene_enumeration/src/tpe/enumeration/own/WuerfelTyp.java
@@ -0,0 +1,41 @@
+package tpe.enumeration.own;
+
+import java.util.Random;
+
+public enum WuerfelTyp {
+
+
+    D4(4), D6(6), D8(8), D10(10), D12(12), D20(20), D22(22);
+
+    private final int anzahlSeiten;
+
+    private WuerfelTyp(int anzahlSeiten){
+        this.anzahlSeiten = anzahlSeiten;
+    }
+
+    public int getAnzahlSeiten(){
+        return anzahlSeiten;
+    }
+
+    public double average(WuerfelTyp A){
+        if(A == D4){
+            return 2.5;
+        } if (A == D6){
+            return 3.5;
+        } if (A == D8){
+            return 4.5;
+        } if (A == D10){
+            return 5.5;
+        } if (A == D12){
+            return 6.5;
+        } if (A==D20){
+            return 10.5;
+        }if (A==D22){
+            return 11.5;
+        }return 0.0;
+    }
+//    private final Random rnd = new Random();
+//    public int roll() {
+//        return rnd.nextInt(anzahlSeiten) + 1;
+//    }
+}
--- 07_enumerationen/02_eigene_enumeration/src/tpe/enumeration/own/test/TestWuerfel.java
+++ 07_enumerationen/02_eigene_enumeration/src/tpe/enumeration/own/test/TestWuerfel.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 import tpe.enumeration.own.Wuerfel;
+import tpe.enumeration.own.WuerfelTyp;
 
 /**
  * Test für die Würfel.
@@ -14,58 +15,25 @@ public class TestWuerfel {
     /** Anzahl der Durchläufe (Gesetz der großen Zahl!). */
     private static final int RUNS = 10000000;
 
-    /**
-     * Testet die Fairness.
-     */
     @Test
-    public void testFairnessD4() {
-        internalTestFairness(new Wuerfel(Wuerfel.D4), 2.5);
-    }
-
-    /**
-     * Testet die Fairness.
-     */
-    @Test
-    public void testFairnessD6() {
-        internalTestFairness(new Wuerfel(Wuerfel.D6), 3.5);
-    }
-
-    /**
-     * Testet die Fairness.
-     */
-    @Test
-    public void testFairnessD8() {
-        internalTestFairness(new Wuerfel(Wuerfel.D8), 4.5);
-    }
-
-    /**
-     * Testet die Fairness.
-     */
-    @Test
-    public void testFairnessD10() {
-        internalTestFairness(new Wuerfel(Wuerfel.D10), 5.5);
-    }
-
-    /**
-     * Testet die Fairness.
-     */
-    @Test
-    public void testFairnessD12() {
-        internalTestFairness(new Wuerfel(Wuerfel.D12), 6.5);
+    public void testAlles(){
+        for(WuerfelTyp würfel : WuerfelTyp.values()){
+            internalTestFairness(new Wuerfel(würfel), würfel.average(würfel));
+        }
     }
 
     /**
      * Interne Hilfsmethode, um die Fairness zu testen.
      *
-     * @param w der zu testende Wuerfel.
+     * @param würfel der zu testende Wuerfel.
      * @param expected Erwartungswert.
      */
-    private void internalTestFairness(Wuerfel w, double expected) {
+    private void internalTestFairness(Wuerfel würfel, double expected) {
 
         long sum = 0;
 
         for (int i = 0; i < RUNS; i++) {
-            sum += w.roll();
+            sum += würfel.roll();
         }
 
         double average = (double) sum / (double) RUNS;
